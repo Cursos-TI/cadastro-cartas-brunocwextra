91a85957d678a99b6db293e9b819b336ab893842
brunocwextra <brunocw.extra@gmail.com>
2025-12-30 19:41:27 +0000

    entrada dps valores das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -26,8 +26,46 @@ int main()
   int pontosturisticos2;
 
   // Área para entrada de dados
+  printf("+-------------------------+\n");
+  printf("|-- Cadastro da Carta 1 --|\n");
+  printf("+-------------------------+\n");
 
-  // Área para exibição dos dados da cidade
+  printf("Estado: ");
+  scanf("%s", estado1);
+  printf("Codigo: ");
+  scanf("%s", codigo1);
+  printf("Cidade: ");
+  scanf("%s", cidade1);
+  printf("Populaçao: ");
+  scanf("%d", &populaçao1);
+  printf("Area: ");
+  scanf("%f", &area1);
+  printf("Pib: ");
+  scanf("%f", &pib1);
+  printf("Pontos Turisticos: ");
+  scanf("%d", &pontosturisticos1);
 
+  printf("\n\n");
+
+  printf("+-------------------------+\n");
+  printf("|-- Cadastro da Carta 2 --|\n");
+  printf("+-------------------------+\n");
+
+  printf("Estado: ");
+  scanf("%s", estado2);
+  printf("Codigo: ");
+  scanf("%s", codigo2);
+  printf("Cidade: ");
+  scanf("%s", cidade2);
+  printf("Populaçao: ");
+  scanf("%d", &populaçao2);
+  printf("Area: ");
+  scanf("%f", &area2);
+  printf("Pib: ");
+  scanf("%f", &pib2);
+  printf("Pontos Turisticos: ");
+  scanf("%d", &pontosturisticos2);
+
+  
   return 0;
 }
